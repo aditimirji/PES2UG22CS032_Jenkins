a1f685af25aa2d85dddf7df92e69e9a8d91badd2
Aditi <142011179+aditimirji@users.noreply.github.com>
2025-03-12 18:29:45 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
+  cout << "Hello, World from PES2UG22CS032\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run this  \n";
+  cout << "I have successfully built and run this HI I AM ADITI \n";
   return 0;
 }
